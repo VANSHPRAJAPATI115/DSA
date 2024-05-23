fcbdc724893d39c4711acb684550f4e1c9c986bd
Vansh Prajapati <Vanshprajapati115@gmail.com>
2024-05-23 08:52:20 +0530

    add sum code

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int sum = 0;
+    for(int i=0; i<n+1; i++){
+        sum = sum + i;
+
+    }
+    cout<<"Sum is :-"<<sum<<endl;
+}
