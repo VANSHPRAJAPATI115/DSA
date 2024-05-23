f980788d7ac72722d821201a2d13ab198dc0e2f9
Vansh Prajapati <Vanshprajapati115@gmail.com>
2024-05-23 11:40:28 +0530

    inverted half cyramid pattern

---
+++ inverted_half_cyramid_pattern.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n, m;
+    cout<<"Enter Row :";
+    cin>>n;
+    cout<<endl;
+
+    cout<<"Enter Col :";
+    cin>>m;
+    cout<<endl;
+
+    for(int i=1; i<=n; i++){
+        for (int j = 1; j <= m; j++)
+        {
+        cout<<"*";
+        }
+        m=m-1;
+        cout<<endl;
+    }
+    return 0;
+}
