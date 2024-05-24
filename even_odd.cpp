f7c8e364e3523596ce5c692fe2c1cdfa033250ae
Vansh Prajapati <Vanshprajapati115@gmail.com>
2024-05-24 09:58:37 +0530

    Odd Even

---
+++ even_odd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter :";
+    cin>>n;
+    if(n%2==0){
+        cout<<"Number Even";
+
+    }
+    else{
+        cout<<"Number Odd";
+    }
+    return 0;
+}
