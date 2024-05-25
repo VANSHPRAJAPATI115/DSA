54c520f43ebb2bfc98b046afa32178f46a6e4daa
Vansh Prajapati <Vanshprajapati115@gmail.com>
2024-05-25 21:28:55 +0530

    reverse array

--- array/reverse_array.cpp
+++ array/reverse_array.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 using namespace std;
 
-    bool search(int arr[], int n, int key){
-        
+    void reverse(int arr[], int n){
+        int start = 0;
+        int end = n-1;
         for (int i = 0; i < n; i++)
         {
-            if(arr[i]==key){
-                return true;
+            if(start<=end){
+                swap(arr[start],arr[end]);
+                start++;
+                end--;
             }
+                    cout<<arr[i];
+
            
         }
-        
-        return false;
+    
     }
 
 int main(){
@@ -19,21 +23,14 @@ int main(){
     cout<<"Enter Size = ";
     cin>>n;
     int arr[n];
-    cout<<"Enter Key you have find sequence = ";
-    cin>>key;
     cout<<"Enter Array Value = ";
     for (int i = 0; i < n; i++)
     {
       cin>>arr[i];      
       
     }
-   int temp= search(arr,n,key);
-     if(temp==true){
-        cout<<"key found";
-     }
-     else{
-        cout<<"Not found";
-     }
+   reverse(arr,n);
+     
     
     return 0;
 }
