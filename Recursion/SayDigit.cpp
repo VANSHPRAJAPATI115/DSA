2a7c7f9ab744b6be0479f9ff4d28eec0739ad6a6
Vansh Prajapati <Vanshprajapati115@gmail.com>
2024-05-30 09:19:34 +0530

    convert number into string

---
+++ Recursion/SayDigit.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+void sayDigit(string arr[],int n){
+    //base cond.
+    if(n==0){
+        return;
+    }
+
+    int digit = n%10;
+    n=n/10;
+    // cout<<arr[digit]<<" ";
+    sayDigit(arr,n);
+
+    cout<<arr[digit]<<" ";
+
+
+
+}
+int main(){
+    int n;
+    cout<<"Enter value n= ";
+    cin>>n;
+    string arr[10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+    sayDigit(arr,n);
+
+    return 0;
+}
