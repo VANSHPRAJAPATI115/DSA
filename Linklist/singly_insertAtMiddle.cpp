7c06b8425d90aec920e86eea87c86c1eb30c5b81
Vansh Prajapati <Vanshprajapati115@gmail.com>
2024-06-02 11:23:14 +0530

    update middle

---
+++ Linklist/singly_insertAtMiddle.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void insertAtHead(Node* &head, int d) {
+    Node* temp = new Node(d);
+    temp->next = head;
+    head = temp;
+}
+
+void insertAtTail(Node* &tail, int d) {
+    Node* temp = new Node(d);
+    tail->next = temp;
+    tail = tail->next;
+}
+
+void insertAtPosition(Node* &tail, Node* &head, int position, int d) {
+    if (position == 1) {
+        insertAtHead(head, d);
+        return;
+    }
+
+    Node* temp = head;
+    int cnt = 1;
+
+    while (cnt < position - 1 && temp != NULL) {
+        temp = temp->next;
+        cnt++;
+    }
+
+    // If the position is beyond the last node, insert at the tail
+    if (temp == NULL || temp->next == NULL) {
+        insertAtTail(tail, d);
+    } else {
+        Node* middle = new Node(d);
+        middle->next = temp->next;
+        temp->next = middle;
+    }
+}
+
+void print(Node* head) {
+    Node* point = head;
+    while (point != NULL) {
+        cout << point->data << " ";
+        point = point->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Node* node1 = new Node(10);
+    Node* head = node1;
+    Node* tail = node1;
+
+    insertAtHead(head, 12);
+   
+    insertAtTail(tail, 15);
+    print(head);
+     insertAtPosition(tail, head, 4, 22);
+    print(head);
+
+    
+
+    cout << "Head data: " << head->data << endl;
+    cout << "Tail data: " << tail->data << endl;
+
+    return 0;
+}
