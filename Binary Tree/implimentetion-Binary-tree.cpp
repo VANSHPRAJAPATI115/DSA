1fb90ae14f4e79daf045982476af2d740ab6905b
Vansh Prajapati <Vanshprajapati115@gmail.com>
2024-06-05 11:27:47 +0530

    implimentation binary tree

---
+++ Binary Tree/implimentetion-Binary-tree.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class node {
+    public:
+        int data;
+        node* left;
+        node* right;
+
+        node(int d) {
+            this->data = d;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+node* buildTree() {
+    cout << "Enter the data: " << endl;
+    int data;
+    cin >> data;
+    if (data == -1) {
+        return NULL;
+    }
+
+    node* root = new node(data);
+    cout << "Enter data for inserting in left of " << data << endl;
+    root->left = buildTree();
+    cout << "Enter data for inserting in right of " << data << endl;
+    root->right = buildTree();
+
+    return root;
+}
+
+int main() {
+    node* root = NULL;
+    root = buildTree();
+    return 0;
+}
