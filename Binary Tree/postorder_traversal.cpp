ce1a7e1d58cb0bea987545db3ab21cccfa74e55d
Vansh Prajapati <Vanshprajapati115@gmail.com>
2024-06-06 08:43:29 +0530

    postorder traversal

---
+++ Binary Tree/postorder_traversal.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class node {
+    public:
+        int data;
+        node* left;
+        node* right;
+
+        node(int d) {
+            this->data = d;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+node* buildTree() {
+    cout << "Enter the data: " << endl;
+    int data;
+    cin >> data;
+    if (data == -1) {
+        return NULL;
+    }
+
+    node* root = new node(data);
+    cout << "Enter data for inserting in left of " << data << endl;
+    root->left = buildTree();
+    cout << "Enter data for inserting in right of " << data << endl;
+    root->right = buildTree();
+
+    return root;
+}
+
+void postorder(node *root){
+    if(root == NULL){
+        return ;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+
+
+}
+
+int main() {
+    node* root = NULL;
+    root = buildTree();
+    cout<<"printing inorder traversal level"<<endl;
+    postorder(root);
+    return 0;
+}
+//1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
