c67a4b47ab00b7a5420174ff8c07d3fe7ec662b2
Vansh Prajapati <Vanshprajapati115@gmail.com>
2024-06-16 14:14:27 +0530

    more program right fib_seq

--- fib_sequence.cpp
+++ fib_sequence.cpp
@@ -18,3 +18,37 @@ int main(){
     
     return 0;
 }
+
+//using recursion and run only platform
+int fib(int n){
+    if(n==0)
+    return 0;
+     
+     if(n==1)
+     return 1;
+
+     int ans = fib(n-1)+fib(n-2);
+     return ans;
+}
+
+
+//call with function
+
+// Function to calculate the Fibonacci number using recursion
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n;
+    std::cout << "Enter the position of the Fibonacci sequence you want to find: ";
+    std::cin >> n;
+
+    int result = fibonacci(n);
+    std::cout << "Fibonacci number at position " << n << " is " << result << std::endl;
+
+    return 0;
+}
