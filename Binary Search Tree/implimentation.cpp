613ea61c6966b033f38075bc00b215c024095a21
Vansh Prajapati <Vanshprajapati115@gmail.com>
2024-06-17 14:24:06 +0530

    insertion of the nodes in binary search tree (BST)

---
+++ Binary Search Tree/implimentation.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int d) {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void levelOrderTraversal(Node* root) {
+    if (root == NULL) {
+        return;
+    }
+
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty()) {
+        Node* temp = q.front();
+        q.pop();
+
+        if (temp == NULL) {
+            cout << endl;
+            if (!q.empty()) {
+                q.push(NULL);
+            }
+        } else {
+            cout << temp->data << " ";
+            if (temp->left) {
+                q.push(temp->left);
+            }
+            if (temp->right) {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+Node* insertIntoBST(Node* root, int data) {
+    if (root == NULL) {
+        root = new Node(data);
+        return root;
+    }
+    if (data > root->data) {
+        root->right = insertIntoBST(root->right, data);
+    } else {
+        root->left = insertIntoBST(root->left, data);
+    }
+    return root;
+}
+
+Node* takeInput() {
+    Node* root = NULL;
+    int data;
+    cin >> data;
+    while (data != -1) {
+        root = insertIntoBST(root, data);
+        cin >> data;
+    }
+    return root;
+}
+
+int main() {
+    Node* root = NULL;
+    cout << "Enter the data (enter -1 to stop):" << endl;
+    root = takeInput();
+    cout << "Printing tree: " << endl;
+    levelOrderTraversal(root);
+
+    return 0;
+}
